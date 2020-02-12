f009d383607aa244ba75cb380de0538d45036f41
Kilian Drechsler <Kilian.Drechsler@hs-augsburg.de>
2020-02-12 11:00:56 +0100

    test publish and recieve
    with args mock

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ import json
 AllowedActions = ['both', 'publish', 'subscribe']
 
 mode = 'subscribe'
-
+#args mock:
+class args:
+    pass
+args.message = 'test publish and subscribe'
 # Custom MQTT message callback
 def customCallback(client, userdata, message):
     print("Received a new message: ")
