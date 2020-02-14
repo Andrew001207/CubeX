d45c9487add6447c252561b7abeebbebf2123a35
Kilian Drechsler <Kilian.Drechsler@hs-augsburg.de>
2020-02-14 23:19:23 +0200

    Squashed commit of the following:
    commit b9e322ddd9e2b971fb4566f411eb6f36c59afc57
    Author: Kilian Drechsler <Kilian.Drechsler@hs-augsburg.de>
    Date:   Fri Feb 14 21:45:29 2020 +0200
    
        deleted token
    
    commit d02989915a46918c11b27823d2e8eb1b45830dc5
    Merge: d469c47 8b55828
    Author: Kilian Drechsler <Kilian.Drechsler@hs-augsburg.de>
    Date:   Fri Feb 14 18:31:03 2020 +0100
    
        Merge branch 'max-di' of https://github.com/Andrew001207/CubeX into max-di
    
    commit 8b558289f2febd6e84ab860aaf32285f98e10f42
    Author: Maximilian Diesenbacher <Maximilian.Diesenbacher@hs-augsburg.de>
    Date:   Fri Feb 14 10:54:40 2020 +0100
    
        new approach
    
    commit 80b5b1c444b6207139b76b65a40ae2b41ba78226
    Author: Maximilian Diesenbacher <Maximilian.Diesenbacher@hs-augsburg.de>
    Date:   Fri Feb 14 10:30:15 2020 +0100
    
        new approach
    
    commit d469c47651894730c12f07b7d98c46e0b090031d
    Author: Kilian Drechsler <Kilian.Drechsler@hs-augsburg.de>
    Date:   Thu Feb 13 11:30:06 2020 +0100
    
        trying to make conversation with user
    
    commit 462386b6e0825b9f57c7b5a1acdc260f6e0ea09d
    Author: Kilian Drechsler <Kilian.Drechsler@hs-augsburg.de>
    Date:   Thu Feb 13 10:42:51 2020 +0100
    
        created empty structur
    
        for task, cube and context handeling from bot
    
    commit 65f79bbad17d3ce603cc1e871b68407aeb971a43
    Merge: ea79f2e 4cde970
    Author: Kilian Drechsler <Kilian.Drechsler@hs-augsburg.de>
    Date:   Thu Feb 13 10:27:31 2020 +0100
    
        Merge branch 'kili/bot' into max-di
    
        fixed bug with credetials
    
    commit 4cde9705af2625d2b5876677945cd2adb37577f7
    Author: Kilian Drechsler <Kilian.Drechsler@hs-augsburg.de>
    Date:   Thu Feb 13 10:17:04 2020 +0100
    
        fixed credetials bug
    
    commit ea79f2e7f3f942b871b7b610ffc657f2c66a9d87
    Merge: c155db5 3fbe04b
    Author: Kilian Drechsler <Kilian.Drechsler@hs-augsburg.de>
    Date:   Thu Feb 13 10:09:35 2020 +0100
    
        Merge branch 'kili/bot' into max-di
    
        added configparser to seperate credetails from code
    
    commit c155db5174a9ee65ba1fc90ec90d7af566186a96
    Author: Maximilian Diesenbacher <Maximilian.Diesenbacher@hs-augsburg.de>
    Date:   Thu Feb 13 10:07:22 2020 +0100
    
        Example Bot added
    
    commit 3fbe04b0cb3168e1e9538bb7c81d816de03a669e
    Author: Kilian Drechsler <Kilian.Drechsler@hs-augsburg.de>
    Date:   Thu Feb 13 10:06:07 2020 +0100
    
        added configparser
    
        to seperate the credetials
    
    commit 8092bc493fadb0909ceea33a915bfdd3b7285641
    Author: Kilian Drechsler <Kilian.Drechsler@hs-augsburg.de>
    Date:   Thu Feb 13 10:04:23 2020 +0100
    
        updated gitignore
    
        with file of bot credetials:
    
        Bots/.bot.conf
    
    commit 0e51d1b34c6bfa36bd7600d881bbf882682fb9a3
    Author: Maximilian Diesenbacher <Maximilian.Diesenbacher@hs-augsburg.de>
    Date:   Thu Feb 13 09:29:48 2020 +0100
    
        New Branch
    
    commit de24d043f23e88c834b803ffe53847951cc89ebd
    Author: Andrew001207 <andrey001207@gmail.com>
    Date:   Thu Feb 13 10:15:19 2020 +0200
    
        Bot example added
    
    commit 34fc531980f0711871cd0af23b0de171d67920df
    Author: Andrew001207 <andrey001207@gmail.com>
    Date:   Wed Feb 12 11:27:47 2020 +0200
    
        fixed sending
    
    commit 9fd65b4c619547394752e1e7e940536a154fb2cd
    Author: Andrew001207 <andrey001207@gmail.com>
    Date:   Wed Feb 12 11:23:01 2020 +0200
    
        Example code added
    
    commit 4ad2eda522c48e9ac0720ef10e4ab8582ca4fc9f
    Author: Andrew001207 <andrey001207@gmail.com>
    Date:   Wed Feb 12 11:01:30 2020 +0200
    
        Added test file

---
+++ Bots/cubeX_TestBot.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to reply to Telegram messages.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackQueryHandler
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def create_task(update, context):
+    """Create a new task for the Cube"""
+    task = ""
+    group = ""
+    def i_start(update, context):
+        update.message.reply_text("Enter name")
+        return SET_NAME
+
+    def i_name(update, context):
+        task = update.message.text
+        update.message.reply_text("Enter group")
+        return SET_GROUP
+
+    def i_group(update, context):
+        group = update.message.text
+        update.message.reply_text("Done")
+        return ConversationHandler.END
+    #task = ask_user(update, context, "Please enter task name")
+    #group = ask_user(update, context, "Got it! Now enter the group of the task")
+    #update.message.reply_text(f'Created task {task} of group {group}.')
+    START, SET_NAME, SET_GROUP = range(3)
+    conv_handler = ConversationHandler(
+        entry_points=[MessageHandler(Filters.regex('^\/ct$'), i_start)],
+
+        states={
+            SET_NAME: [MessageHandler(Filters.text, i_name)],
+
+            SET_GROUP: [MessageHandler(Filters.text, i_group)]
+        },
+
+        fallbacks=[CommandHandler("error", error)]
+    )
+    context.dispatcher.add_handler(conv_handler)
+    #update.message.reply_text(str(context.dispatcher.handlers))
+    print(update.message.text)
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def ask_user(update, context, question):
+    update.message.reply_text(question)
+    msg_id = update.message.message_id
+    print("Before: ", update.message.message_id, context.bot.getUpdates[-1].update_id)
+    while(msg_id == update.message.message_id):
+        pass
+    print("After: ", update.message.message_id)
+    return update.message.text
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(CommandHandler("ct", create_task))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
