e2d83bc3b2f30b7061a91d5ff75027d9a4f85620
Maximilian Diesenbacher <Maximilian.Diesenbacher@hs-augsburg.de>
2020-02-20 18:17:15 +0100

    Connecting to SQL

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import traceback, logging
 
-from bot import State, Conv_automat, Builder
+from bot import State, Conv_automat
 from cubeX import CubeX
 # Read configfile
 config = configparser.ConfigParser()
@@ -25,97 +25,97 @@ def init_states():
 
     state_list = []
 
-    def start(self, answer, arg_dict, **kwargs):
+    def start(self, answer, arg_dict):
         """this is a method which handles the answer and changes the state"""
         return _return_dict(answer)
     state_list.append(State('Please select a command, for avaiable commands enter "help"', start))
 
-    def help(self, answer, arg_dict, **kwargs):
+    def help(self, answer, arg_dict):
         return _return_dict("start")
     state_list.append(State('help text', help))
 
-    def cancel(self, answer, arg_dict, **kwargs):
+    def cancel(self, answer, arg_dict):
         #TODO Clear builder
         return _return_dict("start")
     state_list.append(State("Command cancelled", cancel))
     
-    def error(update, answer, arg_dict, **kwargs):
+    def error(update, answer, arg_dict):
         """Log Errors caused by Updates."""
         return _return_dict("start")
 
-    def create_task(self, answer, arg_dict, **kwargs):
+    def create_task(self, answer, arg_dict):
         #TODO call db method
         return _return_dict("start", f"Following task was created: {answer}")
     state_list.append(State("Please enter the name for the new task", create_task))
 
-    def create_group(self, answer, arg_dict, **kwargs):
+    def create_group(self, answer, arg_dict):
         #TODO call db method
         return _return_dict("start", f"Following group was created: {answer}")
     state_list.append(State("Please enter the name for the new group", create_group))
 
-    def select_cube(self, answer, arg_dict, **kwargs):
+    def select_cube(self, answer, arg_dict):
         #Replace true with DB method cube exists
-        if True and kwargs[builder]:
-            cubeX = CubeX(answer)
-            return _return_dict("select_task", builder=Builder(cubeX.setTask), cubeX=cubeX)
-        elif True and kwargs[builder]:
-            return _return_dict("start", f"Selcted cube {answer}")
+        if True and arg_dict["result_function"]:
+            cubeX = CubeX(int(answer))
+            return _return_dict("select_task", result_function=cubeX.setTask, cubeX=cubeX)
+        elif True and not arg_dict["result_function"]:
+            cubeX = CubeX(int(answer))
+            return _return_dict("start", f"Selcted cube {answer}", cubeX=cubeX)
         else:
             return _return_dict("select_cube", f"Cube {answer} does not exist, please try again")
     state_list.append(State("Please enter the ID of the cube you want to select", select_cube))
 
-    def select_task(self, answer, arg_dict, **kwargs):
+    def select_task(self, answer, arg_dict):
         """this is a method which handles the answer and changes the state"""
         #Replace true with DB method task exists
-        if True and "builder" in kwargs:
+        if True and arg_dict["result_function"]:
             return _return_dict("select_group", None) 
-        elif False and "builder" in kwargs:
+        elif False and arg_dict["result_function"]:
             return _return_dict("select_task", f"Task {answer} does not exist, please try again")
         else:
             return _return_dict("error", f"How the hell did you do this???")
     state_list.append(State("Please enter the name of the task you want to select", select_task))
 
-    def select_group(self, answer, arg_dict, **kwargs):
+    def select_group(self, answer, arg_dict):
         """this is a method which handles the answer and changes the state"""
         #Replace true with DB method group exists
-        if True and "builder" in kwargs:
+        if True and arg_dict["result_function"]:
             return _return_dict("select_side")
-        elif False and "builder" in kwargs:
+        elif False and arg_dict["result_function"]:
             return _return_dict("select_group", f"Group {answer} does not exist, please try again")
         else:
             return _return_dict("error", f"How the hell did you do this???")
     state_list.append(State("Please enter the name of the group you want to select", select_group))
 
-    def select_side(self, answer, arg_dict, **kwargs):
+    def select_side(self, answer, arg_dict):
         #Replace true with DB method group exists
-        if True and "builder" in kwargs:
+        if True and arg_dict["result_function"]:
             try:
                 build_result = builder.build()
             except Exception e:
                 _return_dict("error", "Something went wrong")
             return _return_dict("start", None) #Any answer from builder instead of None
-        elif False and "builder" in kwargs:
+        elif False and arg_dict["result_function"]:
             return _return_dict("select_side", f"Side {answer} does not exist, please try again")
         else:
             return _return_dict("error", f"How the hell did you do this???")
     state_list.append(State("Please enter the number of the side you want to select", select_side))
 
-    def map_task(self, answer, arg_dict, **kwargs):
+    def map_task(self, answer, arg_dict):
         #TODO DB function map_task instead of none
         if arg_dict["cubeX"]:
-            _return_dict("select_task", None, Builder(arg_dict["cubeX"].set_task))
+            _return_dict("select_task", result_function=arg_dict["cubeX"].set_task, **arg_dict)
         else:
-            _return_dict("select_cube", "No cube selected yet", "Need to build Builder", **arg_dict)
+            _return_dict("select_cube", "No cube selected yet", result_function="To be set", **arg_dict)
     state_list.append(State(None, map_task))
 
     return state_list
 
-def _return_dict(next_state, reply=None, builder=None, **self_return):
+def _return_dict(next_state, reply=None, **self_return):
     print(type(self_return))
     return {
         "next_state": next_state
         "reply": reply
-        "builder": builder
         "return_again": self_return
     }
 
