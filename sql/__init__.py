eabe0ec99bc864b70945e07b3d77cdb120d35408
Florian Wöster <f.woester@gmx.de>
2020-03-02 13:21:55 +0200

    documentation sql_Connector,
    deleted json test method

--- sql/__init__.py
+++ sql/__init__.py
@@ -1,1 +1,7 @@
+"""
+Creating a connection to AWS
+Interaction with the SQL Server on AWS
+Class: SqlConn implements methods to interact with the database
+Sql statements to create and delete the tables
+"""
 
