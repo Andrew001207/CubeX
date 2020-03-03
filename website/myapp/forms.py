22579707b5793cbbbdfee2f04388bf48a4d9012b
Momo-tech <matthiasmoser@freenet.de>
2020-03-03 16:40:37 +0200

    Revert "Revert "messed up git""
    This reverts commit 53b9d317ccb434870562e94e605ce102c77f9d79.

--- website/myapp/forms.py
+++ website/myapp/forms.py
@@ -8,3 +8,7 @@ class TaskForm(forms.Form):
     task_name = forms.CharField(label='task_name')
     task_group = forms.CharField(label='task_group')
     cube_id = forms.IntegerField(label='cube_id')
+class SideForm(forms.Form):
+    task_name = forms.CharField(label='task_name')
+    task_group = forms.CharField(label='task_group')
+    side = forms.IntegerField(label='side')
